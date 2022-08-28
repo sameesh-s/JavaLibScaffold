cb36d2c9979cbd93d6a1fab91ade687e77bb7bd2
sameesh-s <sameesh.fisat@gmail.com>
2022-08-28 15:03:35 +0530

    sample test folder added

---
+++ src/test/java/org/sameesh/MainTest.java
@@ -0,0 +1,18 @@
+package org.sameesh;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("sample test case for scaffolding")
+    public void sampleTest(){
+        String test = "this is test";
+        assertEquals(test, "this is test");
+        //assertEquals(test, "kkkk");
+    }
+
+}
